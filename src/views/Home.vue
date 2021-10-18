9c1dde9ca7e52ac623b72de2dc2e5575249ed99e
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2021-10-18 13:14:43 +0200

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    home
+    <p>my name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  setup() {
+    console.log("setup");
+
+    let name = "mario";
+    let age = 30;
+
+    const handleClick = () => {
+      console.log("youclickedme");
+    };
+
+    return { name, age, handleClick };
+  },
+};
 </script>
